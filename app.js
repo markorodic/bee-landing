7295c37c88e582d21cfc1aacf9cb0c30a68b2572
Marko Rodic <mrmarkorodic@googlemail.com>
2017-03-22 01:17:20 +0000

    added responsiveness

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-var text = ["BBOY", "HOUSE", "WACKING", "TAP", "POPPING"]
-
-var count = 0
-
-var element = document.getElementById("changeText")
-setInterval(function(){
-    element.innerHTML = text[count % text.length]
-        count++
-
-}, 2000)
 
 
